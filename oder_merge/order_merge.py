700cc1d372f46268c456facb359e949abec5e725
zingp <605156398@qq.com>
2018-11-04 13:01:39 +0800

    归并排序

--- oder_merge/order_merge.py
+++ oder_merge/order_merge.py
@@ -1,4 +1,5 @@
 
+import random
 # 归并
 # arr = [1, 3, 5, 7, 2, 6, 9, 10]
 # low = 0
@@ -15,7 +16,6 @@ def merge(array, low, mid, high):
         else:
             tmpl.append(array[j])
             j += 1
-        
     while i <= mid:
         tmpl.append(array[i])
         i += 1
@@ -23,9 +23,21 @@ def merge(array, low, mid, high):
         tmpl.append(array[j])
         j += 1
     array[low: high+1] = tmpl[:]
-    return array
+   
+# low,high分别代表第一个和最后一个元素的索引
+def merge_sort(array, low, high):
+    if low < high:    # 至少有两个元素
+        mid = (low+high) // 2
+        merge_sort(array, low, mid)
+        merge_sort(array, mid+1, high)
+        merge(array, low, mid, high)
+    
 
-import random
+# 测试归并
+# arr = [1, 3, 5, 7, 2, 6, 9, 10]
+# print(merge(arr, 0, 3, len(arr)-1))
 
-arr = [1, 3, 5, 7, 2, 6, 9, 10]
-print(merge(arr, 0, 3, len(arr)-1))
+array = list(range(10000))
+random.shuffle(array)
+merge_sort(array, 0, len(array)-1)
+print(array)
