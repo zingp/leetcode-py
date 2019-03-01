682033c50b122ad187bf98a5731cea74c737752f
zingp <605156398@qq.com>
2019-03-01 19:50:11 +0800

    subsets

---
+++ P078SubSet/README.md
@@ -0,0 +1,13 @@
+"""
+78	Subsets	medium	python、java、c++
+90	Subsets II	medium	python、java、c++
+53	Maximum Subarray	easy	python、java、c++
+325	Maximum Size Subarray Sum Equals k	medium	python、java、c++
+209	Minimum Size Subarray Sum	medium	python、java、c++
+238	Product of Array Except Self	medium	python、java、c++
+152	Maximum Product Subarray	medium	python、java、c++
+239	Sliding Window Maximum	hard	python、java、c++
+295	Find Median from Data Stream	hard	python、java、c++
+228	Summary Ranges	medium	python、java、c++
+163	Missing Ranges	medium	python、java、c++
+"""
---
+++ P078SubSet/sub_set.py
@@ -0,0 +1,35 @@
+"""
+Given a set of distinct integers（不同的整数）, nums, return all possible subsets (the power set).
+Note: The solution set must not contain duplicate subsets.
+Example:
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+"""
+import itertools
+
+class Solution:
+    def subsets(self, nums):
+        ans = []
+        def sub(bits):
+            if len(bits) == len(nums):
+                ans.append(list(itertools.compress(nums, bits)))
+                return
+            sub(bits+[True])
+            sub(bits+[False])
+        sub([])
+        return ans
+
+a = list(itertools.compress([1,2,3,4], [0,1,4,5]))
+li = [1,2,3]
+
+print(Solution().subsets(li))
